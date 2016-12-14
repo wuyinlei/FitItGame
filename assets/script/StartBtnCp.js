2bb1676fb32767ef86c9cec74f47c59a7f328ab2
wuyinlei <wuyinlei19931118@hotmail.com>
2016-12-14 15:02:52 +0800

    完成简单的两个场景

---
+++ assets/script/StartBtnCp.js
@@ -0,0 +1,48 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+
+        ["游戏界面"]:{
+            default: null,
+            type: cc.Prefab
+        }
+    },
+
+    onClick: function(){
+        var canvas = this.node.parent;
+        var action = cc.sequence(
+            cc.scaleTo(0.2,0),
+            cc.callFunc(function(){
+                cc.log("开始游戏");
+                cc.director.loadScene("gameScore");
+                this.node.destory();
+            },this)
+        )
+
+        var anim = this.getComponent(cc.Animation);
+        anim.stop();
+
+        this.node.runAction(action);
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
