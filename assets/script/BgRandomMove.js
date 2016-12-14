9ef43743aa1e4ab8a1072a74d17c4d55048a0ee5
wuyinlei <wuyinlei19931118@hotmail.com>
2016-12-14 17:44:45 +0800

    绘制游戏界面完成

--- assets/script/BgRandomMove.js
+++ assets/script/BgRandomMove.js
@@ -12,11 +12,32 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+
+        ["x轴移动距离"]:1,
+        ["y轴移动距离"]:1,
+        ["移动速度(单位：像素/秒)"]:20,
+        ["随机量"]:10
+    },
+
+    //随机移动
+    randomMove: function(){
+        var dt = cc.pLength(cc.p(this["x轴移动距离"],this["y轴移动距离"]))/ this["移动速度(单位：像素/秒)"];
+        var randomDir = cc.pMult(cc.pNormalize(cc.randomMinus1To1(), cc.randomMinus1To1()),this["随机量"]);
+        
+        var moveDir = cc.pAdd(cc.p(this["x轴移动距离"],this["y轴移动距离"]),randomDir);
+
+        var action = cc.repeatForever(
+            cc.sequence(
+                cc.moveBy(dt,moveDir),
+                cc.moveBy(dt,cc.pMult(moveDir,-1))
+            )
+        )
+        this.node.runAction(action);
     },
 
     // use this for initialization
     onLoad: function () {
-
+        this.randomMove();
     },
 
     // called every frame, uncomment this function to activate update callback
--- assets/script/gameCore/App.js
+++ assets/script/gameCore/App.js
@@ -1,26 +1,18 @@
-cc.Class({
-    extends: cc.Component,
-
-    properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
-    },
-
-    // use this for initialization
-    onLoad: function () {
+var app = {
+    startGame : function(){
 
     },
+}
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+    //加载模块
+    var modPath = [
+        'Util',
+    ]
 
-    // },
-});
+    for(var i in modPath){
+        var path = modPath[i];
+        require(path);
+    }
+
+    module.exports = app;
+
